58888d803ca9b25a3cf8ea0c49914e1cd4cbb37a
NoboruWataya123 <jinxw666@gmail.com>
2021-07-16 19:51:35 +0900

    first commit

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment as env } from 'src/environments/environment';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': env.RAPIDAPIKEY,
+        'x-rapidapi-host': env.RAPIDAPIHOST,
+      },
+      setParams: {
+        key: env.MOVIEDB,
+      }
+    });
+    return next.handle(req);
+  }
+}
