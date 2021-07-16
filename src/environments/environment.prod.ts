e31d9c2e912dc321d87c42a0b06a95ecbe027072
NoboruWataya123 <jinxw666@gmail.com>
2021-07-16 20:02:11 +0900

    wtf

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com',
+  RAPIDAPIKEY: '2e2d54528cmsha6f833343d9ac5fp11be54jsn46d03665bc75',
+  RAPIDAPIHOST: 'rawg-video-games-database.p.rapidapi.com',
+  MOVIEDB: '4abb69f0b9a14d43909c1db5c6fa00f1'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  
+  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com',
+  RAPIDAPIKEY: '2e2d54528cmsha6f833343d9ac5fp11be54jsn46d03665bc75',
+  RAPIDAPIHOST: 'rawg-video-games-database.p.rapidapi.com',
+  MOVIEDB: '4abb69f0b9a14d43909c1db5c6fa00f1'
+};
